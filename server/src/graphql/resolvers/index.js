ab5a75cdf5a6e6e6daee00c75ebf37a7d0a32cca
pds37 <blackxub@naver.com>
2018-07-02 18:38:27 +0900

    init & schema & Tweet & graphql

---
+++ server/src/graphql/resolvers/index.js
@@ -0,0 +1,11 @@
+import TweetResolvers from './tweet-resolvers';
+
+export default {
+  Query: {
+      getTweet: TweetResolvers.getTweet,
+      getTweets: TweetResolvers.getTweets,
+  },
+  Mutation: {
+    createTweet: TweetResolvers.createTweet,
+  }
+}
