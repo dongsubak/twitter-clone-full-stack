674e5f5b9fdbcd2a75ba95768903fd94c159f383
pds37 <blackxub@naver.com>
2018-07-04 12:32:03 +0900

    3 User signup login

--- server/src/graphql/schema.js
+++ server/src/graphql/schema.js
@@ -5,11 +5,26 @@ export default `
       message: String
     }
 
+    type Auth {
+      token: String!
+    }
+
+    type User {
+      _id: ID!
+      username: String
+      email: String!
+      firstName: String
+      lastName: String
+      avatar: String
+      createdAt: Date!
+      updatedAt: Date!
+    }
+
     type Tweet {
       _id: ID!
       text: String!
       createdAt: Date!
-      updatedAt: Date !
+      updatedAt: Date!
     }
 
     type Query {
@@ -21,6 +36,8 @@ export default `
       createTweet(text: String!): Tweet
       updateTweet(_id: ID!, text: String): Tweet
       deleteTweet(_id: ID!): Status
+      signup(email: String!, fullName: String!, password: String!, avatar: String, username: String): Auth
+      login(email: String!, password: String!): Auth
     }
 
     schema {
